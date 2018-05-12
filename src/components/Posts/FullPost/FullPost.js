fed49603510261561ad80ee5fadaf9a3eb162f0a
Mr Modise <modisemorebodi@gmail.com>
2018-05-12 19:53:49 +0200

    initial project commit

---
+++ src/components/Posts/FullPost/FullPost.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class FullPost extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+FullPost.propTypes = {};
+
+export default FullPost;
---
+++ src/components/Posts/NewPost/NewPost.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class NewPost extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+NewPost.propTypes = {};
+
+export default NewPost;
