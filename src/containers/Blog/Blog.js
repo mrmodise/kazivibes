e432a8b6c4cb70cb0dfb54b2bdce3e28e53b9975
Mr Modise <modisemorebodi@gmail.com>
2018-05-15 16:41:59 +0200

    adding react-router links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
+import {Route, Link} from 'react-router-dom';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -11,12 +13,13 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/new-post">New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path={"/"} exact component={Posts}/>
+                <Route path={"/new-post"} exact component={NewPost}/>
             </div>
         );
     }
