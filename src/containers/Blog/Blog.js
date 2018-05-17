e8f52dca271cb0714b592f62a51fb6d16fc6e9da
Mr Modise <modisemorebodi@gmail.com>
2018-05-17 14:03:59 +0200

    adding lazy loading

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,8 +2,12 @@ import React, {Component} from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import {Route, NavLink, Switch} from 'react-router-dom';
-import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
@@ -21,7 +25,7 @@ class Blog extends Component {
                 </header>
                 <Switch>
                     <Route path={"/"} exact component={Posts}/>
-                    <Route path={"/new-post"} component={NewPost}/>
+                    <Route path={"/new-post"} component={AsyncNewPost}/>
                     <Route path={"/:id"} exact component={FullPost}/>
                 </Switch>
             </div>
